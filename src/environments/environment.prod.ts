7f95c900f812e90dc4f974094570a40e5fc188f7
byniched <123072356+byniched@users.noreply.github.com>
2023-01-26 10:47:26 +0100

    Virtual Wallet

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCtc0_8eiivXY_Zgt832vY99wGtdFvqCJA",
+
+    authDomain: "my-wallet-ed7b0.firebaseapp.com",
+
+    projectId: "my-wallet-ed7b0",
+
+    storageBucket: "my-wallet-ed7b0.appspot.com",
+
+    messagingSenderId: "580476790359",
+
+    appId: "1:580476790359:web:242803b61a41aab4366512"
+  }
+
+};
